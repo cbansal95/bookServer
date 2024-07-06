ec449b3261d120191baba7ae2a17750ac395cafd
Chirag Bansal <chirag@contineu.ai>
2024-07-06 12:48:20 +0530

    Added API documentation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -24,22 +24,60 @@ export const resolvers = {
     }
 }
 
+/**
+ * Retrieves a book from the database based on the provided ID.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {number} args.id - The ID of the book to retrieve.
+ * @param {Context} context - The context object containing the Prisma client.
+ * @return {Promise<Book | null>} A promise that resolves to the retrieved book or null if not found.
+ */
 function getBook(_parent: unknown, args: { id: number }, context: Context) {
     const id = Number(args.id)
     validateIdSchema(id)
     return context.prisma.book.findUnique({ where: { 'id': id } })
 }
 
+/**
+ * Retrieves all books from the database.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {} _args - Empty object as no arguments are expected.
+ * @param {Context} context - The context object containing the Prisma client.
+ * @return {Promise<Book[]>} A promise that resolves to an array of books.
+ */
 function getBooks(_parent: unknown, _args: {}, context: Context) {
     return context.prisma.book.findMany()
 }
 
+/**
+ * Retrieves reviews for a specific book.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {number} args.bookId - The ID of the book to retrieve reviews for.
+ * @param {Context} context - The context object containing the Prisma client.
+ * @return {Promise<Review[]>} A promise that resolves to an array of reviews for the specified book.
+ */
 function getReviews(_parent: unknown, args: { bookId: number }, context: Context) {
     const bookId = Number(args.bookId)
     validateIdSchema(bookId)
     return context.prisma.review.findMany({ where: { 'bookId': bookId } })
 }
 
+/**
+ * Logs in a user with the provided email and password.
+ * Maintains auth state by generating and JWT and setting it to the cookie.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {string} args.email - The email of the user.
+ * @param {string} args.password - The password of the user.
+ * @param {Context} context - The context object containing the Prisma client.
+ * @return {Promise<Object>} A promise that resolves to an object with the message "User logged in".
+ * @throws {Error} If the credentials are invalid.
+ */
 async function login(_parent: unknown, args: { email: string, password: string }, context: Context) {
     const { email, password } = args
     validateDecodedUserSchema(args)
@@ -55,6 +93,15 @@ async function login(_parent: unknown, args: { email: string, password: string }
     return {"message": "User logged in"}	
 }
 
+/**
+ * Retrieves reviews for the current user.
+ * Requires authentication.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {Context} context - The context object containing the Prisma client.
+ * @return {Promise<Review[]>} A promise that resolves to an array of reviews for the current user.
+ */
 async function getMyReviews(_parent: unknown, args: {  }, context: Context) {
     const { userId } = context
     if(!userId) {
@@ -63,6 +110,18 @@ async function getMyReviews(_parent: unknown, args: {  }, context: Context) {
     return context.prisma.review.findMany({ where: { userId } })
 }
 
+/**
+ * Registers a new user with the given username, email, and password.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {string} args.username - The username of the user.
+ * @param {string} args.email - The email of the user.
+ * @param {string} args.password - The password of the user.
+ * @param {Context} context - The context object containing the Prisma client.
+ * @return {Promise<Object>} A promise that resolves to an object with the message "User created".
+ * @throws {Error} If the user already exists.
+ */
 async function register(_parent: unknown, args: { username: string, email: string, password: string }, context: Context) {
     const { username, email, password } = args
 
@@ -77,6 +136,19 @@ async function register(_parent: unknown, args: { username: string, email: strin
     return {"message": "User created"}	
 }
 
+/**
+ * Adds a new book to the database.
+ * Requires authentication.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {string} args.title - The title of the book.
+ * @param {string} args.author - The author of the book.
+ * @param {number} args.publishedYear - The published year of the book.
+ * @param {Context} context - The context object containing the Prisma client.
+ * @return {Promise<Object>} A promise that resolves to the created book object.
+ * @throws {Error} If the user is not authenticated.
+ */
 async function addBook(_parent: unknown, args: { title: string, author: string, publishedYear: number }, context: Context) {
     const { userId } = context
     if(!userId) {
@@ -87,6 +159,19 @@ async function addBook(_parent: unknown, args: { title: string, author: string, 
     return context.prisma.book.create({ data: { title, author, publishedYear } })
 }
 
+/**
+ * Adds a review for a book.
+ * Requires authentication.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {number} args.bookId - The ID of the book for which the review is being added.
+ * @param {number} args.rating - The rating given in the review.
+ * @param {string} args.comment - The comment provided in the review.
+ * @param {Context} context - The context object containing the Prisma client.
+ * @return {Promise<Object>} A promise that resolves to the created review object.
+ * @throws {Error} If the user is not authenticated, book is not found, or validation fails.
+ */
 async function addReview(_parent: unknown, args: { bookId: number, rating: number, comment: string }, context: Context) {
     const { userId } = context
     if(!userId) {
@@ -107,6 +192,19 @@ async function addReview(_parent: unknown, args: { bookId: number, rating: numbe
     return context.prisma.review.create({ data: { bookId, userId, rating, comment } })
 }
 
+/**
+ * Updates a review with the provided review ID, rating, and comment.
+ * Requires authentication.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {number} args.reviewId - The ID of the review to be updated.
+ * @param {number} args.rating - The new rating for the review.
+ * @param {string} args.comment - The new comment for the review.
+ * @param {Context} context - The context object containing the Prisma client and user ID.
+ * @return {Promise<Object>} A promise that resolves to the updated review object.
+ * @throws {Error} If the user is not authenticated, review is not found, or user is not authorized to update the review.
+ */
 async function updateReview(_parent: unknown, args: { reviewId: number, rating: number, comment: string }, context: Context) {
     const { userId } = context
     if(!userId) {
@@ -128,6 +226,16 @@ async function updateReview(_parent: unknown, args: { reviewId: number, rating: 
     return context.prisma.review.update({ where: { "id": reviewId }, data: { rating, comment } })
 }
 
+/**
+ * Deletes a review based on the provided review ID.
+ * Requires authentication.
+ *
+ * @param {unknown} _parent - The parent object in the GraphQL query.
+ * @param {Object} args - The arguments passed to the function.
+ * @param {number} args.reviewId - The ID of the review to be deleted.
+ * @param {Context} context - The context object containing the Prisma client and user ID.
+ * @return {Object} A message indicating the deletion of the review.
+ */
 async function deleteReview(_parent: unknown, args: { reviewId: number }, context: Context) {
     const { userId } = context
     if(!userId) {
